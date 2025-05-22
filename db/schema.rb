1deaec8fe59c3e055cf752f54c005ee1612728f3
Ritikesh-StackFusion <ritikeshfrank@gmail.com>
2025-05-22 17:50:57 +0530

    New Link to access Cat index in store index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_22_115817) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_22_120601) do
+  create_table "cats", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stores", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
